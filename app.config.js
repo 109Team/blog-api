d278ec8ce78102e356c9cd3039cbaf227ce20eec
loohopen <loohopen@github.com>
2019-05-06 18:04:40 +0800

    v1.0.1 jwt密钥配置化

--- app.config.js
+++ app.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    VERSION: "v1",
-    MONGODB: {
+    VERSION: "v1",          // API版本号
+    MONGODB: {              // mongodb配置信息
         uri: 'mongodb://127.0.0.1:27017/test'
-    }
+    },
+    JWTSECRET: 'Hopenjwt'   // 开发环境jwt密钥
+    
 }
--- core/auth.js
+++ core/auth.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const resHandle = require('../core/dbResHandle');
-const jwtTokenSecret = 'HopenJwt';
+const CONFIG = require('../app.config');
 
 module.exports = (req, res, next) => {
     const _token = req.headers.authorization;
     if(!_token){
         return resHandle(res, null, 401, '无权访问');
     }
-    jwt.verify(_token, jwtTokenSecret, (err, decode) => {
+    jwt.verify(_token, CONFIG.JWTSECRET, (err, decode) => {
         if(err){
             return resHandle(res, null, 401, '无权访问');
         }else{
