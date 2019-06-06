ce46773d8095afee106096eedf174b196c4a5f8d
loohopen <loohopen@github.com>
2019-06-06 18:05:43 +0800

    v1.0.1 jwt方法修改

--- core/auth.js
+++ core/auth.js
@@ -4,13 +4,14 @@ const resHandle = require('../core/dbResHandle');
 const CONFIG = require('../app.config');
 
 module.exports = (req, res, next) => {
-    const _token = req.headers.Authoriation;
+    const _token = req.headers.authoriation;
     if(!_token){
         return resHandle(res, null, 401, '无权访问');
     }
+    
     jwt.verify(_token, CONFIG.JWTSECRET, (err, decode) => {
         if(err){
-            return resHandle(res, null, 401, '无权访问');
+            return resHandle(res, err, 401, '无权访问');
         }else{
             req.user = decode;
             return next();
